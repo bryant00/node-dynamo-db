6480cc02d7e40c1b23b45472b712721b21cd33e1
Kizmet <bryantpatton@gmail.com>
2019-10-01 14:52:41 -0700

    test db setup

---
+++ app.js
@@ -0,0 +1,49 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var bodyParser = require("body-parser");
+var AWS = require("aws-sdk");
+
+var indexRouter = require("./routes/index");
+var carsRouter = require("./routes/cars");
+
+var app = express();
+app.listen(3000, () => console.log("Cars API listening on port 3000!"));
+AWS.config.update({
+  region: "eu-west-2",
+  endpoint: "http://localhost:8000"
+});
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+// view engine setup
+//app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/cars", carsRouter);
+
+// catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
---
+++ routes/cars.js
@@ -0,0 +1,96 @@
+var express = require("express");
+var router = express.Router();
+var AWS = require("aws-sdk");
+
+AWS.config.update({
+  region: "eu-west-2",
+  endpoint: "http://localhost:8000"
+});
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+router.get("/", function(req, res) {
+  var params = {
+    TableName: "Cars",
+    ProjectionExpression:
+      "#id, #name, #type, #manufacturer, #fuel_type, #description",
+    ExpressionAttributeNames: {
+      "#id": "id",
+      "#name": "name",
+      "#type": "type",
+      "#manufacturer": "manufacturer",
+      "#fuel_type": "fuel_type",
+      "#description": "description"
+    }
+  };
+  console.log("Scanning Cars table.");
+  docClient.scan(params, onScan);
+  function onScan(err, data) {
+    if (err) {
+      console.error(
+        "Unable to scan the table. Error JSON:",
+        JSON.stringify(err, null, 2)
+      );
+    } else {
+      res.send(data);
+      // print all the Cars
+      console.log("Scan succeeded.");
+      data.Items.forEach(function(car) {
+        console.log(car.id, car.type, car.name);
+      });
+      if (typeof data.LastEvaluatedKey != "undefined") {
+        console.log("Scanning for more...");
+        params.ExclusiveStartKey = data.LastEvaluatedKey;
+        docClient.scan(params, onScan);
+      }
+    }
+  }
+});
+
+router.put("/", function(req, res, next) {
+  const car = req.body;
+  var params = {
+    TableName: "Cars",
+    Item: {
+      id: car.id,
+      type: car.type,
+      name: car.name,
+      manufacturer: car.manufacturer,
+      fuel_type: car.fuel_type,
+      description: car.description
+    }
+  };
+  docClient.put(params, function(err, data) {
+    if (err) {
+      res.status(402).send(JSON.stringify(err, null, 2));
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+router.get("/:id", function(req, res) {
+  var carID = parseInt(req.params.id);
+  var params = {
+    TableName: "Cars",
+    KeyConditionExpression: "#id = :id",
+    ExpressionAttributeNames: {
+      "#id": "id"
+    },
+    ExpressionAttributeValues: {
+      ":id": carID
+    }
+  };
+  docClient.query(params, function(err, data) {
+    if (err) {
+      console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+    } else {
+      console.log("Query succeeded.");
+      res.send(data.Items);
+      data.Items.forEach(function(car) {
+        console.log(car.id, car.name, car.type);
+      });
+    }
+  });
+});
+
+module.exports = router;
